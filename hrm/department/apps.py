59260132e08b24d756e54f3e1a34e675d298390e
Pasha-Ignatyuk <kiotocloud@gmail.com>
2021-11-21 21:53:29 +0300

    tests passes

--- hrm/department/apps.py
+++ hrm/department/apps.py
@@ -1,5 +1,7 @@
+"""A registry of installed applications that stores configuration and provides introspection"""
 from django.apps import AppConfig
 
 
 class DepartmentConfig(AppConfig):
+    """The RmConfig.name attribute tells Django that this configuration applies to Rm application"""
     name = 'department'
--- hrm/department/views/views.py
+++ hrm/department/views/views.py
@@ -1,3 +1,1 @@
-from django.shortcuts import render
-
-# Create your views here.
+"""View - is responsible for displaying information (visualization)"""
